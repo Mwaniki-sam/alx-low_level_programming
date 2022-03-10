36711b40a3f2bcf3ba6251f5b8539ffa9f484f4b
Mwaniki-sam <mwanikisamson745@gmail.com>
2022-03-11 01:21:59 +0300

    correctiomn on task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- *main -entry point
-*
-*Return: 0 after printing the function
-*/
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
 int main(void)
 {
-puts("\"programming is like
-puts("\"programming is like building in multilingual puzzle"):
+puts("\"programming is like building a multilingual puzzle"):
 return (0):
 }
