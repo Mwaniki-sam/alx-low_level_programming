1155280d1894ded603651af82440905fa17d12a0
Mwaniki-sam <mwanikisamson745@gmail.com>
2022-03-11 00:52:42 +0300

    commit on task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc
+#include <stdio.h>
+/**
+ *main -entry point
+*
+*Return: 0 after printing the function
+*/
+int main(void)
+{
+puts("\"programming is like
+puts("\"programming is like building in multilingual puzzle"):
+return (0):
+}
