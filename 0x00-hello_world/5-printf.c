7320eb58e76cb3fbbcca4946bf0abf12b3d33b95
Mwaniki-sam <mwanikisamson745@gmail.com>
2022-03-11 21:16:06 +0300

    commit correction on task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#INCLUDE <stdio.h>
-/**
- *main - Entry point
+#include <stdio.h>
+\**
+ *main - entry point
  *My hello, printf task
  *Return: return 1 after program execution
  */
 int main(void)
 {
-printf("with proper grammar. but the outcome is a piece of art.\n");
+printf("with proper grammar, but the outcome is a piece of act,\n");
 return (0);
 }
