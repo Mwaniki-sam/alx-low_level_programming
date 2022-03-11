cecccb6094506954164d98f5e1462606b0aac5ba
Mwaniki-sam <mwanikisamson745@gmail.com>
2022-03-11 20:47:57 +0300

    commit on task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h> 
 /**
  *main - entry point
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle"):
-return (0):
+puts("\"programming is like building multilingual puzzle");
+return (0);
 }
