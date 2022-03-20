c7c6ba637052c8e9f8a1737d739d892cb65e1839
Mwaniki-sam <mwanikisamson745@gmail.com>
2022-03-20 20:15:38 +0300

    my commit task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * largest_number - return the largest of 3 numbers
+ * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
  * Return: largest number
  */
 
-int largets_number(int a, int b, int c)
+int largest_number(int a, int b, int c)
 {
 int largest;
 
@@ -21,8 +21,8 @@ else if (b >= a && b >= c)
 largest = b;
 }
 else
-{
-largest = c;
+{largest = c;
 }
-return (largest);
+
+return = (largest);
 }
